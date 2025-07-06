be07ed40517acec6f0c664f5e1728a1827b97f3f
Aiden Powell <powell.ai@northeastern.edu>
2025-07-06 00:47:57 -0400

    Refactor project, start tests

--- aiden_halfstack_solution/src/scorer.py
+++ aiden_halfstack_solution/src/scorer.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 from math import sqrt
 from rapidfuzz import fuzz
 
+from upload_table import remove_table
+
+
 # Additional config options must be added in
 # 1) the proper type method
 # 2) the SCORING_REGISTRY
@@ -20,6 +23,7 @@ class Scorer(ABC):
         """
         pass
 
+
 class NumberScorer(Scorer):
     def score(self, request_val, candidate_val, use_global_max=True, max_val=None, **_) -> float:
         """
@@ -30,43 +34,61 @@ class NumberScorer(Scorer):
             return 0.0
 
         diff = abs(request_val - candidate_val)
-        max_val = max_val if use_global_max else max(request_val, candidate_val)
+        max_val = max_val if use_global_max and max_val else max(request_val, candidate_val)
         return max(0.0, 1.0 - diff / max_val)
 
+
 class StringScorer(Scorer):
-    def score(self, request_val, candidate_val, threshold=80) -> float:
+    def score(self, request_val, candidate_val, threshold=80, exact_match=False) -> float:
         """
         :param threshold: minimum fuzz ratio [0..100] for scoring; ratios above the threshold are linearly mapped onto [0..1]
+        :param exact_match: toggle for only succeeding the string match if they are identical. Equivalent to threshold=100
         """
         if request_val is None or candidate_val is None:
             return 0.0
 
-        request_val, candidate_val = _clean_string(request_val), _clean_string(request_val)
+        request_val = _clean_string(request_val)
+        candidate_val = _clean_string(candidate_val)
+
+        if exact_match or threshold == 100:
+            return request_val == candidate_val
+
         raw_ratio = fuzz.ratio(request_val, candidate_val)
         if raw_ratio < threshold:
             return 0.0
         return (raw_ratio - threshold) / (100 - threshold)
 
+
 class TupleScorer(Scorer):
     def score(self, request_val, candidate_val, distance_mode="euclidean", tolerance=10.0, **_) -> float:
         """
         :param distance_mode: euclidean distance is more sensitive to large differences in a single dimension; manhattan difference treats all dimensions equally
-        :param tolerance: assures a perfect match within the tolerance, and gradually decays outside of it
+        :param tolerance: assures a perfect match within the tolerance, and gradually decays outside of it. Higher tolerance recommended for manhattan searches
         """
-        request_val_list = _parse(request_val)
-        candidate_val_list = _parse(request_val), _parse(request_val)
 
-        # check they're actually numbers
+        if request_val is None or candidate_val is None:
+            return 0.0
+
+        request_val = _parse(request_val)
+        candidate_val = _parse(candidate_val)
+
+        # fail if input aren't numbers
         try:
             request_val_list = [float(x) for x in request_val]
+        except (TypeError, ValueError):
+            raise ValueError("Improper input")
+
+        # return 0 if candidates aren't numbers
+        try:
             candidate_val_list = [float(x) for x in candidate_val]
         except (TypeError, ValueError):
             return 0.0
 
-        # return 0 for empty set
-        n = min(len(request_val_list), len(candidate_val_list))
-        if n == 0.0:
-            return 0.0
+        # fail for unmatched dimensions
+        n = len(request_val_list)
+        if n != len(candidate_val_list):
+            raise ValueError("Improper input")
+
 
         # distance calculation by mode
         if distance_mode == "manhattan":
@@ -88,7 +110,10 @@ class ListScorer(Scorer):
         """
         :param match_mode: jaccard more heavily penalizes differences between sets, while overlap mode just checks for inclusion
         """
-        request_val, candidate_val = _parse(request_val), _parse(request_val)
+
+        if request_val is None or candidate_val is None:
+            return 0.0
+
         set_r, set_c = set(request_val), set(candidate_val)
 
         if not set_r or not set_c:
@@ -98,6 +123,7 @@ class ListScorer(Scorer):
         if match_mode == "overlap":
             return len(intersect) / len(set_r)
         return len(intersect) / len(set_r | set_c)
+
 
 class BooleanScorer(Scorer):
     def score(self, request_val, candidate_val, **_) -> float:
@@ -123,7 +149,7 @@ SCORING_REGISTRY = {
 # add new default configs here
 SCORING_CONFIG = {
     "number": {"use_global_max": True},
-    "string": {"threshold": 80},
+    "string": {"threshold": 80, "exact_match": False},
     "tuple": {"distance_mode": "euclidean", "tolerance": 10.0},
     "list": {"match_mode": "jaccard"},
     "boolean": {}
